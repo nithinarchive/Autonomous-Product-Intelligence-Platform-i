6530676a67ef4b620e10af3455253e6492f48474
nithinarchive <nithinsureshk11@gmail.com>
2025-12-18 13:44:55 +0530

    Create services.py

---
+++ services.py
@@ -0,0 +1,7 @@
+def get_insights():
+    return {
+        "retention_drop": True,
+        "cause": "Checkout redesign",
+        "confidence": 0.87,
+        "recommended_action": "Rollback feature"
+    }
